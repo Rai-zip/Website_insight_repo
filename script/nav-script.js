197f780b3f87e2559042fd37af345381686a6df0
Anugrah Rai <raianugrah2004@gmail.com>
2024-05-19 23:58:16 -0400

    changed the javascript scroll function to match open wherever the window is

--- script/nav-script.js
+++ script/nav-script.js
@@ -7,7 +7,12 @@ function closeNav() {
 
 function openNav() {
     let sidebar = document.getElementById('side-bar');
+    let scrollOffset = window.scrollY; // Get the current vertical scroll position
+
+    // Adjust the position of the sidebar based on the scroll offset
+    sidebar.style.top = `${scrollOffset}px`;
     sidebar.style.right = '0';
     document.body.classList.add('no-scroll');
 }
+
 

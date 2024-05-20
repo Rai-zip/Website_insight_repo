580e3eb594dc3ef593274cfcc9b0824a14fa2e45
Anugrah Rai <raianugrah2004@gmail.com>
2024-05-19 23:53:50 -0400

    making my side bar and navbar work

--- script/nav-script.js
+++ script/nav-script.js
@@ -2,9 +2,12 @@
 function closeNav() {
     let sidebar = document.getElementById('side-bar');
     sidebar.style.right = '-100%'; // Move the sidebar completely out of view
+    document.body.classList.remove('no-scroll');
 }
 
 function openNav() {
     let sidebar = document.getElementById('side-bar');
     sidebar.style.right = '0';
+    document.body.classList.add('no-scroll');
 }
+
